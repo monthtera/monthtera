5fb9dc4aecd79e537ae601956cd63d25873d2aaa
June Park <laurenceyoon@gmail.com>
2019-08-04 23:48:57 +0900

    modify user models

--- users/models.py
+++ users/models.py
@@ -4,4 +4,16 @@ from django.db import models
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    pass
+    email = models.CharField(
+        max_length=255,
+        verbose_name='email',
+        unique=True,
+    )
+    phone_number = models.CharField(max_length=11, null=True)
+    date_of_birth = models.DateTimeField(null=True)
+    gender = models.CharField(max_length=1, null=True)
+    address = models.CharField(max_length=44, null=True)
+    enable_alarm = models.BooleanField(default=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
